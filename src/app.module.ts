c99024250d344627ab44f0bfb33dd0690c5bb851
tardio <tardio717@gmail.com>
2022-11-07 14:45:11 -0400

    home

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersController } from './users/users.controller';
+import { ClienteController } from './cliente/cliente.controller';
+import { VehiculosController } from './vehiculos/vehiculos.controller';
+import { PedidosController } from './pedidos/pedidos.controller';
+
+@Module({
+  imports: [],
+  controllers: [AppController, UsersController, ClienteController, VehiculosController, PedidosController],
+  providers: [AppService],
+})
+export class AppModule {}
