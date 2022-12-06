4e306b4d742e25a398ff187e4e76427e68ff80dc
tardio <tardio717@gmail.com>
2022-12-06 14:51:32 -0400

    finalizado

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,15 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
 @Controller('auth')
 export class AuthController {
   constructor(private authservice: AuthService) {}
+  //registarse
   @Post('/signin')
   signin(@Body() dto: AuthDto) {
     return this.authservice.signin(dto);
   }
+  // incribirse
   @Post('/signup')
   singup() {
     this.authservice.signup();
--- src/tarea/tarea.controller.ts
+++ src/tarea/tarea.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { TareaService } from './tarea.service';
 
 @Controller('tarea')
-export class TareaController {}
+export class TareaController {
+  constructor(private tareaservice: TareaService) {}
+  @Get()
+  obtener() {
+    return this.tareaservice.obtener();
+  }
+  @Post()
+  registrar(@Body() dto: Prisma.tareaCreateInput) {
+    return this.tareaservice.registrar(dto);
+  }
+}
